ed60572f329e5bcf2169aa659e3fe424a32d103c
kahenteikou <kokkiemouse@gmail.com>
2022-03-30 13:38:36 +0900

    fixed

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+import json
+import datetime
+import time
+
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+@app.route("/")
+def index():
+    return "doutei"
 def main():
     print("hello work")
+    app.debug=True
+    host="localhost"
+    port=5454
+    host_port=(host,port)
+    server=WSGIServer(
+        host_port,
+        app
+    )
+    server.serve_forever()
 if __name__ == '__main__':
     main()
